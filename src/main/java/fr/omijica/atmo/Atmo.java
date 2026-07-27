f5b604e20ff8ebeaf43c562d036cb92cfe5748b0
omijica <100131434+omijica@users.noreply.github.com>
2026-07-27 17:30:29 +0200

    Initial commit

---
+++ src/main/java/fr/omijica/atmo/Atmo.java
@@ -0,0 +1,17 @@
+package fr.omijica.atmo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Atmo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
